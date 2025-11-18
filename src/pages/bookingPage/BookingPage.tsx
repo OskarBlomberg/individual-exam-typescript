ee9e03e3cf8c748c64ed5685698dd8cf782c6b2d
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-11-18 14:43:58 +0100

    styling loadingPage and adding form

--- src/pages/bookingPage/BookingPage.tsx
+++ src/pages/bookingPage/BookingPage.tsx
@@ -1,5 +1,85 @@
 import "./bookingPage.scss";
+import { Logotype } from "../../components/logotype/Logotype";
 
 export const BookingPage = () => {
-  return <h1>BookingPage</h1>;
+  return (
+    <main className="booking">
+      <section className="booking__logo">
+        <Logotype>
+          <h1 className="booking__logo__heading">booking</h1>
+          <></>
+        </Logotype>
+      </section>
+
+      <form className="booking__form" action="handleSubmit">
+        <fieldset className="booking__form__subsection booking__form__subsection--general">
+          {
+            <legend className="booking__form__subsection__title ">
+              when, what & who
+            </legend>
+          }
+
+          <label
+            className="subsection__label subsection__label--date"
+            htmlFor="date"
+          >
+            date
+          </label>
+          <input
+            className="subsection__input subsection__input--date"
+            type="date"
+            name="date"
+            id="date"
+            required
+            aria-required
+          />
+
+          <label
+            className="subsection__label subsection__label--time"
+            htmlFor="time"
+          >
+            time
+          </label>
+          <input
+            className="subsection__input subsection__input--time"
+            type="time"
+            name="time"
+            id="time"
+            required
+            aria-required
+          />
+
+          <label
+            className="subsection__label subsection__label--people"
+            htmlFor="people"
+          >
+            number of awesome bowlers
+          </label>
+          <input
+            className="subsection__input subsection__input--people"
+            type="number"
+            name="people"
+            id="people"
+            required
+            aria-required
+          />
+
+          <label
+            className="subsection__label subsection__label--lanes"
+            htmlFor="lanes"
+          >
+            number of lanes
+          </label>
+          <input
+            className="subsection__input subsection__input--lanes"
+            type="number"
+            name="lanes"
+            id="lanes"
+            required
+            aria-required
+          />
+        </fieldset>
+      </form>
+    </main>
+  );
 };
--- src/pages/loadingPage/LoadingPage.tsx
+++ src/pages/loadingPage/LoadingPage.tsx
@@ -3,10 +3,10 @@ import { Logotype } from "../../components/logotype/Logotype";
 
 export const LoadingPage = () => {
   return (
-    <main className="logoMain">
+    <main className="loadingMain">
       <Logotype>
-        <h1 className="logoMain__heading">strajk</h1>
-        <h2 className="logoMain__subtitle">bowling</h2>
+        <h1 className="loadingMain__heading">strajk</h1>
+        <h2 className="loadingMain__subtitle">bowling</h2>
       </Logotype>
     </main>
   );
